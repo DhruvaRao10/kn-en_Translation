db826589dc7065e2e17bcfb6e479b4d73c125dd1
DhruvaRao10 <dhruvabantwalrao@gmail.com>
2025-08-16 09:39:57 +0530

    production build prune

--- server/main.py
+++ server/main.py
@@ -100,5 +100,5 @@ async def translate_text(request: TextTranslationRequest):
     )
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
